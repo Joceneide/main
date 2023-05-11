6056f02b527b86876b569b5d5e7a66aa7d913607
Joceneide <131299803+Joceneide@users.noreply.github.com>
2023-05-11 09:39:18 -0300

    Add files via upload

--- Exercicio/main.js
+++ Exercicio/main.js
@@ -1,23 +1,34 @@
-const formulario = document.querySelector('form');
+const nomeBeneficiario = document.getElementById('nome_beneficiario');
+let formValido = false;
+const form = document.getElementById('form_dep');
 
 
-formulario.addEventListener('submit', function(event) {
-  event.preventDefault(); 
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
 
-const campoA = document.querySelector('#campoa');
-const campoB = document.querySelector('#campob');
-const mensagemSucesso = 'Formulário válido.'
-const mensagemFalha = 'Formulário inválido. O campo B deve ser maior que o campo A.'
-
-formValido = (campoB.value > campoA.value)
-
-if (formValido) {
-    alert (mensagemSucesso);
-} else {
-    alert (mensagemFalha);
+    return nomeComoArray.length >= 2;
 }
-});
 
+form.addEventListener('submit', function(e){
 
+    e.preventDefault();
 
+    const numeroConta = document.getElementById('numero_conta');
+    const valorDeposito = document.getElementById('valor_deposito');
+    const mensagemSucesso = `Valor de: <b>${valorDeposito.value}</b> foi depositado para o cliente <b>${nomeBeneficiario.value}</b> - conta nº: <b>${numeroConta.value}</b>`
 
+    formValido = validaNome(nomeBeneficiario.value);
+
+    if(formValido){
+        const ContainerMensagemSucesso = document.querySelector('.mensagem_sucesso');
+        ContainerMensagemSucesso.innerHTML = mensagemSucesso;
+        ContainerMensagemSucesso.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroConta.value = '';
+        valorDeposito.value = '';
+    }else{
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.mensagem_erro').style.display = 'block';
+    }
+})
