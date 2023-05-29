6d6ce741a7715db5c34a85da1a7d09cfc315ef72
Aluno <jofreitas29@gmail.com>
2023-05-29 17:23:54 -0300

    new file

--- Exercicio/Calculadora/main.js
+++ Exercicio/Calculadora/main.js
@@ -1,22 +1,26 @@
-let displayValue = '';
-
-function appendToDisplay(value) {
-    displayValue += value;
-    document.getElementById('display').value = displayValue;
-}
-
-function calculate() {
-    let result = '';
-    try {
-        result = eval(displayValue);
-    } catch (error) {
-        result = 'Erro';
-    }
-    document.getElementById('display').value = result;
-    displayValue = result;
-}
-
-function clearDisplay() {
-    displayValue = '';
-    document.getElementById('display').value = '';
-}
+function insert(num)
+        {
+            var numero = document.getElementById('resultado').innerHTML;
+            document.getElementById('resultado').innerHTML = numero + num;
+        }
+        function clean()
+        {
+            document.getElementById('resultado').innerHTML = "";
+        }
+        function back()
+        {
+            var resultado = document.getElementById('resultado').innerHTML;
+            document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+        }
+        function calcular()
+        {
+            var resultado = document.getElementById('resultado').innerHTML;
+            if(resultado)
+            {
+                document.getElementById('resultado').innerHTML = eval(resultado);
+            }
+            else
+            {
+                document.getElementById('resultado').innerHTML = "Nada..."
+            }
+        }
